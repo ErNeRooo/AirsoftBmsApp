c60802388c8c13fa5ce0f48d492e782400b61927
ErNeRooo <erneroooooo@gmail.com>
2025-05-13 18:18:43 +0200

    refactor SignUpPage

--- AirsoftBmsApp/View/Pages/SignUpPage.xaml.cs
+++ AirsoftBmsApp/View/Pages/SignUpPage.xaml.cs
@@ -9,9 +9,4 @@ public partial class SignUpPage : ContentPage
 		InitializeComponent();
         BindingContext = viewModel;
     }
-
-    private async void OnBackButtonClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("..");
-    }
 }
