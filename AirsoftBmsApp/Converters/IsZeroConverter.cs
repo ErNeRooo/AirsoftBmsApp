b4ebe4093b55ea1514f3fdfa470c196d61a193b2
ErNeRooo <erneroooooo@gmail.com>
2025-05-30 15:16:22 +0200

    add No Players message in TeamView

---
+++ AirsoftBmsApp/Converters/IsZeroConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftBmsApp.Converters
+{
+    public class IsZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool canBeParsed = int.TryParse(value?.ToString(), out int intValue);
+
+            if (!canBeParsed) throw new Exception("Value can't be converted to integer");
+
+            return intValue == 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
