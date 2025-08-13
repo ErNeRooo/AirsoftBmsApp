951cff04995219791814d5cb51852c117bd5843b
ErNeRooo <erneroooooo@gmail.com>
2025-08-13 13:08:30 +0200

    refactor BattlePage

---
+++ AirsoftBmsApp/View/ContentViews/Battle/BattlePreview.xaml.cs
@@ -0,0 +1,11 @@
+using AirsoftBmsApp.ViewModel.BattleViewModel;
+
+namespace AirsoftBmsApp.View.ContentViews.Battle;
+
+public partial class BattlePreview : ContentView
+{
+	public BattlePreview()
+	{
+		InitializeComponent();
+    }
+}
